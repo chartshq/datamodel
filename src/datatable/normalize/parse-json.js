91a2c0c6881188ba4596cc42ae248782953b2c93
Subhash Halder <subhash@fusioncharts.com>
2017-09-14 11:48:16 +0530

    FSB-8: Support for json in normalizer is done

--- src/datatable/normalize/parse-json.js
+++ src/datatable/normalize/parse-json.js
@@ -1,10 +1,26 @@
+import formatCell from './cell-formatter';
 /**
  * This helps to convert user JSON data to dataTable consumable json format
  * @param  {json} data The user JSON data in string format
+ * @param  {json} schema Schema of the data
  * @return {json}      The json format required by DataTable
  */
-function parseJSON(data) {
-    return data;
+function parseJSON(data, schema) {
+    const retJson = {
+        schema,
+        data: new Array(schema.length),
+    };
+    const retJsonData = retJson.data;
+
+    // iterating through the data row
+    data.forEach((row) => {
+        // Iterating through the data column
+        schema.forEach((col, ii) => {
+            // If measure convert it to Number
+            (retJsonData[ii] || (retJsonData[ii] = [])).push(formatCell(row[col.name], col));
+        });
+    });
+    return retJson;
 }
 
 export { parseJSON as default };
---
+++ src/datatable/normalize/parse-json.spec.js
@@ -0,0 +1,97 @@
+/* global describe, it */
+/* eslint-disable no-unused-expressions */
+
+import { expect } from 'chai';
+import parseJSON from './parse-json';
+
+describe('Checking parseJSON', () => {
+    it('valid Data ', () => {
+        const jsonData = [
+            { a: 10, aaa: 20, aaaa: 'd' },
+            { a: 15, aaa: 25, aaaa: 'demo' },
+        ];
+        const schema = [
+            { name: 'a', type: 'measure' },
+            { name: 'aaa', type: 'measure' },
+            { name: 'aaaa', type: 'dimension' },
+        ];
+        const retArr = {
+            schema,
+            data: [
+                [10, 15],
+                [20, 25],
+                ['d', 'demo'],
+            ],
+        };
+        expect(parseJSON(jsonData, schema)).to.deep.equal(retArr);
+    });
+    it('valid Data with date', () => {
+        const jsonData = [
+            { a: '2012-1', aaa: 20, aaaa: 'd' },
+            { a: '2012-2', aaa: 25, aaaa: 'demo' },
+        ];
+        const schema = [
+            { name: 'a', type: 'datetime', format: '%Y-%m' },
+            { name: 'aaa', type: 'measure' },
+            { name: 'aaaa', type: 'dimension' },
+        ];
+        const retArr = {
+            schema,
+            data: [
+                [new Date(2012, 0), new Date(2012, 1)],
+                [20, 25],
+                ['d', 'demo'],
+            ],
+        };
+        expect(parseJSON(jsonData, schema)).to.deep.equal(retArr);
+    });
+    it('valid extra schema Data ', () => {
+        const jsonData = [
+            { a: 10, aaa: 20, aaaa: 'd' },
+            { a: 15, aaa: 25, aaaa: 'demo' },
+        ];
+        const schema = [
+            { name: 'a', type: 'measure' },
+            { name: 'aaa', type: 'measure' },
+            { name: 'aaaa', type: 'dimension' },
+            { name: 'aaaaa', type: 'dimension' },
+        ];
+        const retArr = {
+            schema,
+            data: [
+                [10, 15],
+                [20, 25],
+                ['d', 'demo'],
+                ['', ''],
+            ],
+        };
+        expect(parseJSON(jsonData, schema)).to.deep.equal(retArr);
+    });
+    it('valid extra Data row', () => {
+        const jsonData = [
+            { a: 10, aaa: 20, aaaa: 'd' },
+            { a: 15, aaa: 25, aaaa: 'demo' },
+        ];
+        const schema = [
+            { name: 'a', type: 'measure' },
+            { name: 'aaa', type: 'measure' },
+        ];
+        const retArr = {
+            schema,
+            data: [
+                [10, 15],
+                [20, 25],
+            ],
+        };
+        expect(parseJSON(jsonData, schema)).to.deep.equal(retArr);
+    });
+    it('blank Data ', () => {
+        const jsonData = [];
+        const schema = [];
+        const retArr = {
+            schema,
+            data: [],
+        };
+        expect(parseJSON(jsonData, schema)).to.deep.equal(retArr);
+    });
+});
