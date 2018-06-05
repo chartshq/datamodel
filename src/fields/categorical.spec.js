901122c2f2a4153b4f5c4f7d77690d9ccc800fe1
Rousan Ali <rousanali786@gmail.com>
2018-06-05 20:10:14 +0530

    FSB-358: Write test cases for field type classes

---
+++ src/fields/categorical.spec.js
@@ -0,0 +1,25 @@
+/* global describe, it, beforeEach */
+/* eslint-disable no-unused-expressions */
+
+import { expect } from 'chai';
+import { DimensionSubtype } from 'picasso-util';
+import Categorical from './categorical';
+
+describe('Categorical Field Type', () => {
+    const schema = {
+        name: 'Country',
+        type: 'dimension',
+        subtype: DimensionSubtype.CATEGORICAL
+    };
+
+    const data = ['India', 'USA', 'Japan', 'China', 'India', 'Japan'];
+    let field;
+
+    beforeEach(() => {
+        field = new Categorical(schema.name, data, schema);
+    });
+
+    it('should implement getter methods', () => {
+        expect(field.subType()).to.equal(schema.subtype);
+    });
+});
