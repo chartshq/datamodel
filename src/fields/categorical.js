68818c141191796e2aa58ca53b2651aab7f6a4da
Rousan Ali <rousanali786@gmail.com>
2018-06-11 17:47:04 +0530

    FSB-280: Fix typo and comments for field types

--- src/fields/categorical.js
+++ src/fields/categorical.js
@@ -8,13 +8,22 @@ import Dimension from './dimension';
  */
 class Categorical extends Dimension {
 
+    /**
+     * Creates new Categorical field instance.
+     *
+     * @param {string} name - The name of the field.
+     * @param {Array} data - An array containing the field data.
+     * @param {Object} schema - The schema for the field.
+     */
     constructor(name, data, schema) {
         super(name, data, schema);
         this.subtype = DimensionSubtype.CATEGORICAL;
     }
 
     /**
-    * @returns {String} SubType of field
+     * Getter for subType value of the field.
+     *
+    * @return {string} Returns subType of the field.
     */
     subType() {
         return this.subtype;
