49efe526ad4e82f218896233f0152c9bafe45b4c
Rousan Ali <rousanali786@gmail.com>
2018-06-27 17:50:37 +0530

    FSB-390: Add build config for dev mode

---
+++ webpack.config.dev.js
@@ -0,0 +1,21 @@
+const libraryName = 'DataTable';
+const outFile = 'datatable.js';
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: `${__dirname}/dist`,
+        filename: outFile,
+        library: libraryName,
+        libraryTarget: 'umd',
+        umdNamedDefine: true,
+    },
+    devtool: 'source-map',
+    module: {
+        rules: []
+    },
+    devServer: {
+        inline: true,
+        contentBase: './example',
+    },
+};
