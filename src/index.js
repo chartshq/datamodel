4cd31fba4f9edfc413b01027e3bdd1d22a8d3e45
Akash Goswami <akash@fusioncharts.com>
2018-07-13 17:36:11 +0530

    Export type as named component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
-import { DataFormat, DimensionSubtype, FieldType, ProjectionMode, SelectionMode } from 'picasso-util';
 import DataModel from './datamodel';
 import * as Operators from './operator';
 
 DataModel.Operators = Operators;
 
 export default DataModel;
-
-export const TYPES = {
-    DataFormat,
-    DimensionSubtype,
-    FieldType,
-    SelectionMode,
-    ProjectionMode
-};
+export { DataFormat, DimensionSubtype, FieldType, ProjectionMode, SelectionMode } from 'picasso-util';
