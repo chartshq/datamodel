17b8522b15fceabf00afdb73780b167e38a4dedb
Ujjal Kumar Dutta <ujjal@fusioncharts.com>
2018-08-15 18:58:36 +0530

    CDC-11: Expose Data Format and filtering options

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import DataModel from './datamodel';
 import * as Operators from './operator';
 import * as Stats from './stats';
 import pkg from '../package.json';
+import { DataFormat, FilteringMode } from './constants';
 
 DataModel.Operators = Operators;
 DataModel.Stats = Stats;
+DataModel.DataFormat = DataFormat;
+DataModel.FilteringMode = FilteringMode;
 DataModel.version = pkg.version;
 
 export default DataModel;
